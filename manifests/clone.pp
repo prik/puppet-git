3a65c9681655b2830667429405dd4c95063f5a36
Erika <er@mobinvest.com>
2013-07-17 19:08:47 +0200

    initial base commit

---
+++ manifests/clone.pp
@@ -0,0 +1,15 @@
+###############################################
+# usage:
+# git::clone { "name": repository => "https://giturl/name.git" }
+#
+##############################################
+
+define git::clone($repository,
+  $path = '/vagrant') {
+
+  exec { "git clone ${repository}":
+    cwd     => $path,
+    creates => "${path}/${name}",
+    require => Class[ 'composer' ]
+  }
+}
